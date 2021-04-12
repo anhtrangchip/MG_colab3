9a4a7ace9689b65495c0e045b42ef3206b3e9335
anhtrangchip <anhtrangchip@gmail.com>
2021-04-12 07:47:34 +0700

    batch size 16

--- run.py
+++ run.py
@@ -52,7 +52,7 @@ def main():
     training_sequences = pipeline.encoded_sequences['training']
     validation_sequences = pipeline.encoded_sequences['validation']
     
-    batch_size = 8
+    batch_size = 16
     print("batch size: ", batch_size)
     
     train(transformer, training_sequences, validation_sequences,
